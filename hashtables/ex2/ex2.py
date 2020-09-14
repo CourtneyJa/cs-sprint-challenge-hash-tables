9a15bf238247c1bd98f5dbdcf23ba9174ae1ac3e
CourtneyJa <courtney.jackson08234@gmail.com>
2020-09-13 21:40:51 -0400

    completed ex2 and passing test

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -9,6 +9,9 @@ def reconstruct_trip(tickets, length):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    hashTable = {ticket.source: ticket.destination for ticket in tickets}
+    route = [hashTable["NONE"]]
+    for _ in range(length-1):
+        route.append(hashTable[route[-1]])
 
     return route
